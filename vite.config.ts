60549cae48b283b0bf25e76f372daabb526a0ee4
Matt Kibbler <mattkibbler@gmail.com>
2024-10-01 16:30:50 +0100

    Use project version to specify base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import path from "path";
+const { version } = require("./package.json");
 
-export default defineConfig({
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+export default ({ mode }) => {
+	return {
+		base: mode === "github" ? `/v${version}/` : "./",
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
 		},
-	},
-});
+	};
+};
