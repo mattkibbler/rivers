20e5c95fd03218e5a0ee680ea952c98b20b68a9c
Matt Kibbler <mattkibbler@gmail.com>
2024-10-01 16:36:15 +0100

    Missed repo name out of github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import path from "path";
 const { version } = require("./package.json");
 
+const repoName = "rivers";
 export default ({ mode }) => {
 	return {
-		base: mode === "github" ? `/version-${version}/` : "./",
+		base: mode === "github" ? `/${repoName}/version-${version}/` : "./",
 		resolve: {
 			alias: {
 				"@": path.resolve(__dirname, "./src"),
